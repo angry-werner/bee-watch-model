ed596895773ff88f91ba144ef2fa532607bd2a1e
kow <werner.koch@bluewin.ch>
2017-11-06 21:25:57 +0100

    (kow) Getter erstellt.

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,11 @@
 export class Bee {
-    constructor(name: string) {}
+    private _name: string;
+
+    constructor(name: string) {
+        this._name = name;
+    }
+
+    get name(): string {
+        return this._name;
+    }
 }
