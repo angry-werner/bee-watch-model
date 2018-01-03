04a3568da802a67cdd24e01c85549dd4e2c74693
Werner Koch <werner.koch@bluewin.ch>
2018-01-03 21:48:29 +0100

    (kow) Fixed index.

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,7 @@
-// Everything is in modules
+export * from './beewatch/bees/Bee';
+export * from './beewatch/bees/Colony';
+export * from './beewatch/bees/Subspecies';
+export * from './beewatch/environment/BeeHive';
+export * from './beewatch/environment/Coordinate';
+export * from './beewatch/environment/Location';
+export * from './beewatch/environment/Location';
